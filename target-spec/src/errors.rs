8970f72992dfc59786124869beae632f76089875
Rain <rain@sunshowers.io>
2022-10-23 21:18:37 -0700

    [target-spec] expand ExpressionParseError, add support for miette 5
    Going to use this in nextest.

--- target-spec/src/errors.rs
+++ target-spec/src/errors.rs
@@ -6,7 +6,7 @@
 use std::{borrow::Cow, error, fmt};
 
 /// An error that happened during `target-spec` parsing or evaluation.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum Error {
     /// A `cfg()` expression was invalid and could not be parsed.
@@ -19,6 +19,7 @@ pub enum Error {
     ///
     /// This is no longer used, but is kept for backwards compatibility.
     #[deprecated(since = "1.1.0", note = "this variant is no longer in use")]
+    #[doc(hidden)]
     UnknownPredicate(String),
 }
 
@@ -50,34 +51,142 @@ impl error::Error for Error {
     }
 }
 
+// Note: ExpressionParseError is a duplicate of cfg_expr::error::ParseError, and is copied here
+// because we don't want to expose that in a stable (1.0+) API.
+
 /// An error returned in case a `TargetExpression` cannot be parsed.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
+#[non_exhaustive]
 pub struct ExpressionParseError {
-    inner: cfg_expr::ParseError,
+    /// The string we tried to parse.
+    pub input: String,
+
+    /// The range of characters in the original string that resulted
+    /// in this error.
+    pub span: std::ops::Range<usize>,
+
+    /// The kind of error that occurred.
+    pub kind: ExpressionParseErrorKind,
 }
 
 impl ExpressionParseError {
-    pub(crate) fn new(inner: cfg_expr::ParseError) -> Self {
-        Self { inner }
+    pub(crate) fn new(input: &str, error: cfg_expr::ParseError) -> Self {
+        // The error returned by cfg_expr::ParseError does not include the leading 'cfg('. Use the
+        // original input and add 4 which is the length of 'cfg('.
+        let span = if input.starts_with("cfg(") && input.ends_with(')') {
+            (error.span.start + 4)..(error.span.end + 4)
+        } else {
+            error.span
+        };
+        Self {
+            input: input.to_owned(),
+            span,
+            kind: ExpressionParseErrorKind::from_cfg_expr(error.reason),
+        }
     }
 }
 
 impl fmt::Display for ExpressionParseError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "expression parse error")
+        write!(f, "error parsing cfg() expression")
     }
 }
 
-impl error::Error for ExpressionParseError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        Some(&self.inner)
+impl error::Error for ExpressionParseError {}
+
+/// The kind of [`ExpressionParseError`] that occurred.
+#[derive(Clone, Debug, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum ExpressionParseErrorKind {
+    /// not() takes exactly 1 predicate, unlike all() and any()
+    InvalidNot(usize),
+    /// The characters are not valid in an cfg expression
+    InvalidCharacters,
+    /// An opening parens was unmatched with a closing parens
+    UnclosedParens,
+    /// A closing parens was unmatched with an opening parens
+    UnopenedParens,
+    /// An opening quotes was unmatched with a closing quotes
+    UnclosedQuotes,
+    /// A closing quotes was unmatched with an opening quotes
+    UnopenedQuotes,
+    /// The expression does not contain any valid terms
+    Empty,
+    /// Found an unexpected term, which wasn't one of the expected terms that
+    /// is listed
+    Unexpected {
+        /// The list of expected terms.
+        expected: &'static [&'static str],
+    },
+    /// Failed to parse an integer value
+    InvalidInteger,
+    /// The root cfg() may only contain a single predicate
+    MultipleRootPredicates,
+    /// A `target_has_atomic` predicate didn't correctly parse.
+    InvalidHasAtomic,
+    /// An element was not part of the builtin information in rustc
+    UnknownBuiltin,
+}
+
+impl ExpressionParseErrorKind {
+    fn from_cfg_expr(reason: cfg_expr::error::Reason) -> Self {
+        use cfg_expr::error::Reason::*;
+
+        match reason {
+            InvalidCharacters => Self::InvalidCharacters,
+            UnclosedParens => Self::UnclosedParens,
+            UnopenedParens => Self::UnopenedParens,
+            UnclosedQuotes => Self::UnclosedQuotes,
+            UnopenedQuotes => Self::UnopenedQuotes,
+            Empty => Self::Empty,
+            Unexpected(expected) => Self::Unexpected { expected },
+            InvalidNot(np) => Self::InvalidNot(np),
+            InvalidInteger => Self::InvalidInteger,
+            MultipleRootPredicates => Self::MultipleRootPredicates,
+            InvalidHasAtomic => Self::InvalidHasAtomic,
+            UnknownBuiltin => Self::UnknownBuiltin,
+        }
+    }
+}
+
+impl fmt::Display for ExpressionParseErrorKind {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use ExpressionParseErrorKind::*;
+
+        match self {
+            InvalidCharacters => f.write_str("invalid character(s)"),
+            UnclosedParens => f.write_str("unclosed parens"),
+            UnopenedParens => f.write_str("unopened parens"),
+            UnclosedQuotes => f.write_str("unclosed quotes"),
+            UnopenedQuotes => f.write_str("unopened quotes"),
+            Empty => f.write_str("empty expression"),
+            Unexpected { expected } => {
+                if expected.len() > 1 {
+                    f.write_str("expected one of ")?;
+
+                    for (i, exp) in expected.iter().enumerate() {
+                        f.write_fmt(format_args!("{}`{exp}`", if i > 0 { ", " } else { "" }))?;
+                    }
+                    f.write_str(" here")
+                } else if !expected.is_empty() {
+                    f.write_fmt(format_args!("expected a `{}` here", expected[0]))
+                } else {
+                    f.write_str("the term was not expected here")
+                }
+            }
+            InvalidNot(np) => f.write_fmt(format_args!("not() takes 1 predicate, found {np}")),
+            InvalidInteger => f.write_str("invalid integer"),
+            MultipleRootPredicates => f.write_str("multiple root predicates"),
+            InvalidHasAtomic => f.write_str("expected integer or \"ptr\""),
+            UnknownBuiltin => f.write_str("unknown built-in"),
+        }
     }
 }
 
 /// An error returned while parsing a single target.
 ///
 /// This is caused by a triple not being understood by either `cfg-expr` or `target-lexicon`.
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct TripleParseError {
     triple_str: Cow<'static, str>,
     lexicon_err: cfg_expr::target_lexicon::ParseError,
@@ -109,5 +218,24 @@ impl fmt::Display for TripleParseError {
 impl error::Error for TripleParseError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         Some(&self.lexicon_err)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::TargetExpression;
+    use test_case::test_case;
+
+    #[test_case("cfg()", 4..4; "empty expression results in span inside cfg")]
+    #[test_case("target_os = \"macos", 12..18; "unclosed quote specified without cfg")]
+    fn test_expression_parse_error_span(input: &str, expected_span: std::ops::Range<usize>) {
+        let err = match TargetExpression::new(input).unwrap_err() {
+            Error::InvalidExpression(err) => err,
+            other => {
+                panic!("unexpected error type {other:?}");
+            }
+        };
+        assert_eq!(err.span, expected_span);
     }
 }
